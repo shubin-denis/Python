e6781074955299bd3608108fbfc048c20e8c45e5
Denis <pro166@mail.ru>
2020-10-24 12:36:24 +0300

    task 4-5

---
+++ Lesson_1/task_4.py
@@ -0,0 +1,18 @@
+# Пользователь вводит целое положительное число. Найдите самую большую цифру в числе.
+# Для решения используйте цикл while и арифметические операции.
+
+user_answer = int(input('Введите целое положительное число: '))
+remains = user_answer%10
+user_answer = user_answer//10
+
+while user_answer > 0:
+    if user_answer%10 > remains:
+        remainsremains = user_answer%10
+    user_answer = user_answer//10
+print(f'Наибольшее число: {remains}')
+
+
+
+
+
+
