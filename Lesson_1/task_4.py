b6923f288057fd5b6d45677647bc2bfeeb129cd9
Denis <pro166@mail.ru>
2020-11-05 16:59:26 +0300

    Lesson_1 update

--- Lesson_1/task_4.py
+++ Lesson_1/task_4.py
@@ -2,17 +2,11 @@
 # Для решения используйте цикл while и арифметические операции.
 
 user_answer = int(input('Введите целое положительное число: '))
-remains = user_answer%10
-user_answer = user_answer//10
+remains = user_answer % 10
+user_answer = user_answer // 10
 
 while user_answer > 0:
-    if user_answer%10 > remains:
-        remainsremains = user_answer%10
-    user_answer = user_answer//10
+    if user_answer % 10 > remains:
+        remains = user_answer % 10
+    user_answer = user_answer // 10
 print(f'Наибольшее число: {remains}')
-
-
-
-
-
-
